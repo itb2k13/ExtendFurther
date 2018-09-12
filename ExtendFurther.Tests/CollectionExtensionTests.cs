d8be57e6f74914473c7624b3920563151c19553e
Stephen Smith <itcombox@gmail.com>
2018-09-12 02:59:27 +0100

    - adding unit tests

---
+++ ExtendFurther.Tests/CollectionExtensionTests.cs
@@ -0,0 +1,18 @@
+﻿using ExtendIt;
+using NUnit.Framework;
+using System.Linq;
+
+namespace ExtendFurther.Tests
+{
+    class CollectionExtensionTests
+    {
+        [TestCase(new string[] { "", "" }, false)]
+        [TestCase(new string[] { "" }, true)]
+        [TestCase(new string[] { "a", "b" }, true)]
+        [TestCase(new string[] { "a", "b", "b" }, false)]
+        public void ExtensionListExtensionDeterminesIfItemsInListAreDistinct(string[] s, bool isDistinct)
+        {
+            Assert.That(s.ToList().IsDistinct, Is.EqualTo(isDistinct));
+        }
+    }
+}
--- ExtendFurther.Tests/StringExtensionTests.cs
+++ ExtendFurther.Tests/StringExtensionTests.cs
@@ -1,21 +1,51 @@
 ﻿using ExtendIt;
 using NUnit.Framework;
+using System.Linq;
 
 namespace ExtendFurther.Tests
 {
 
-        [TestFixture]
-        public class ExtendFurtherTests
+    [TestFixture]
+    public class ExtendFurtherTests
     {
-            [Test]
-            [TestCase("", false)]
-            [TestCase(null, false)]
-            [TestCase(" ", false)]
-            [TestCase("abc", true)]
-            public void Extension_StringExtension_Exists(string s, bool expectedOutcome)
-            {
-                Assert.That(s.Exists(), Is.EqualTo(expectedOutcome));
-            }
+        [Test]
+        [TestCase("", false)]
+        [TestCase(null, false)]
+        [TestCase(" ", false)]
+        [TestCase("abc", true)]
+        public void Extension_StringExtension_Exists(string s, bool expectedOutcome)
+        {
+            Assert.That(s.Exists(), Is.EqualTo(expectedOutcome));
         }
 
+        [Test]
+        [TestCase("", 3, "")]
+        [TestCase(null, 3, "")]
+        [TestCase("abc", 3, "abc")]
+        [TestCase("a", 3, "a")]
+        [TestCase("abcdefgh", 5, "abcde")]
+        [TestCase("abc", 0, "")]
+        [TestCase("a", -1, "")]
+        public void Extension_StringExtension_FirstN_Returns_First_N_Chars_Of_String(string s, int n, string expectedResult)
+        {
+            Assert.That(s.FirstN(n), Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        [TestCase("", "", true)]
+        [TestCase(null, null, true)]
+        [TestCase("abc", "ABC", true)]
+        [TestCase("123", "123", true)]
+        [TestCase("", "123", false)]
+        [TestCase("abc", "def", false)]
+        [TestCase(null, "def", false)]
+        [TestCase("abc", null, false)]
+        public void Extension_StringExtension_IsEqualTo(string s, string t, bool expectedResult)
+        {
+            Assert.That(s.EqualTo(t), Is.EqualTo(expectedResult));
+        }
+
+    }
 }
+
+
