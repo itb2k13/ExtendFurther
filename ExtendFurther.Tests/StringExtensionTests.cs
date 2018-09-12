33ab1c7e158fc0bfa1ba7f8f689691524224ca98
Stephen Smith <itcombox@gmail.com>
2018-09-12 02:54:29 +0100

    - add unit test project - downgrade to vs2015

---
+++ ExtendFurther.Tests/StringExtensionTests.cs
@@ -0,0 +1,21 @@
+﻿using ExtendIt;
+using NUnit.Framework;
+
+namespace ExtendFurther.Tests
+{
+
+        [TestFixture]
+        public class ExtendFurtherTests
+    {
+            [Test]
+            [TestCase("", false)]
+            [TestCase(null, false)]
+            [TestCase(" ", false)]
+            [TestCase("abc", true)]
+            public void Extension_StringExtension_Exists(string s, bool expectedOutcome)
+            {
+                Assert.That(s.Exists(), Is.EqualTo(expectedOutcome));
+            }
+        }
+
+}
