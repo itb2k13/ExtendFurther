2d03019b3c685b2f0daf5e2870594cd6de0fff27
Stephen Smith <itcombox@gmail.com>
2018-09-12 10:36:14 +0100

    - more unit tests

--- ExtendFurther.Tests/NumericalExtensionTests.cs
+++ ExtendFurther.Tests/NumericalExtensionTests.cs
@@ -17,7 +17,35 @@ namespace ExtendFurther.Tests
         {
             Assert.That(i.ToBool(), Is.EqualTo(expectedOutcome));
         }
-        
+
+        [Test]
+        [TestCase(0, 0)]
+        [TestCase(0.00, 0)]
+        [TestCase(1.449999999, 1)]
+        [TestCase(1.3, 1)]
+        [TestCase(1.53423432, 2)]
+        [TestCase(1.555555, 2)]
+        [TestCase(-1.555555, -2)]
+        public void Extension_NumericalExtension_Round0Dp_Rounds_To_Zero_Decimal_Places(decimal d, decimal expectedOutcome)
+        {
+            Assert.That(d.Round0Dp(), Is.EqualTo(expectedOutcome));
+        }
+
+        [Test]
+        [TestCase(0, 0, 0)]
+        [TestCase(0.00, 0, 0)]
+        [TestCase(1.53423432, 0, 2)]
+        [TestCase(1.555555, 0, 2)]
+        [TestCase(-1.555555, 0, -2)]
+        [TestCase(0, 1, 0.0)]
+        [TestCase(0.00, 1, 0.00)]
+        [TestCase(1.53423432, 1, 1.5)]
+        [TestCase(1.555555, 1, 1.6)]
+        [TestCase(-1.555555, 1, -1.6)]
+        public void Extension_NumericalExtension_Rnd_Rounds_To_Expected_Decimal_Places(decimal d, int decimalPlaces, decimal expectedOutcome)
+        {
+            Assert.That(d.Rnd(decimalPlaces), Is.EqualTo(expectedOutcome));
+        }
     }
 }
 
