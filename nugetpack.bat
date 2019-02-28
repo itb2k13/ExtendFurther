d74030904b3a07711e84b8f8c2d8b0b601d90851
Stephen Smith <itcombox@gmail.com>
2019-02-28 23:27:58 +0000

    Delete nugetpack.bat

--- nugetpack.bat
+++
@@ -1,4 +0,0 @@
-del "D:\Repos\Personal\ExtendFurther\*.nupkg"
-d:\nuget.exe pack "D:\Repos\Personal\ExtendFurther\package.nuspec" -OutputDirectory "D:\Repos\Personal\ExtendFurther\
-d:\nuget.exe push "D:\Repos\Personal\ExtendFurther\*.nupkg" oy2jnpgog5kmi42dvoha3ifrlqzf53tena7kdntbz7752y -Source https://api.nuget.org/v3/index.json
-pause
